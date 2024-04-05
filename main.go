96ca4b0ead2a45a85d1f8386cd8498cf52054917
cvs <cvscvscvs473@gmail.com>
2024-04-05 18:48:58 +0300

    fixed MatchOrders and output functions

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ var incr int64 = 0
 var incrPtr = &incr
 
 type order struct {
-	user_id, order_id, amount, price int64
-	side                             bool // true for buy, false for sell
+	user_id, order_id, amount, init_amount, price int64
+	side                                          bool // true for buy, false for sell
 }
 
 type orderBook struct {
@@ -71,39 +71,81 @@ func (order *order) MatchOrders(ob *orderBook, users *users) {
 				if order.amount < sellOrd.amount {
 					remaining_amount := sellOrd.amount - order.amount
 					total_price := sellOrd.price * order.amount
-					for _, u := range *users { // changing assets
+					for k, u := range *users { // changing assets
 						if u.user_id == order.user_id {
 							u.assets["UAH"] += order.amount
 							u.assets["USD"] -= total_price
 							u.completedOrders = append(u.completedOrders, *order)
+							(*users)[k].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if order.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[k].activeOrders = u.activeOrders
 						}
 						if u.user_id == sellOrd.user_id {
 							u.assets["USD"] += total_price
 							u.assets["UAH"] -= order.amount
+							sellOrd.amount = remaining_amount
+							for ind, o := range (*users)[k].activeOrders {
+								if o.order_id == sellOrd.order_id {
+									(*users)[k].activeOrders[ind] = sellOrd
+								}
+							}
 						}
 					}
-					sellOrd.amount = remaining_amount
-					fmt.Printf("Buy order complete: bought %v UAH for %v USD each.\n", order.amount, sellOrd.price)
-					ob.SellOrders[i-completed_orders] = sellOrd
+					if len(ob.SellOrders) > 1 {
+						ob.SellOrders[i-completed_orders] = sellOrd
+					} else {
+						ob.SellOrders[0] = sellOrd
+					}
+					fmt.Printf("\nBuy order complete: bought %v UAH for %v USD each.\n", order.amount, sellOrd.price)
 					ob.DelOrder(*order)
+					completed_orders++
 					ob.print()
 					break
 				} else if order.amount == sellOrd.amount {
 					total_price := sellOrd.price * order.amount
-					for _, u := range *users { // changing assets
+					for i, u := range *users { // changing assets
 						if u.user_id == order.user_id {
 							u.assets["UAH"] += order.amount
 							u.assets["USD"] -= total_price
 							u.completedOrders = append(u.completedOrders, *order)
-						}
-						if u.user_id == sellOrd.user_id {
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if order.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
+						} else if u.user_id == sellOrd.user_id {
 							u.assets["USD"] += total_price
 							u.assets["UAH"] -= order.amount
-							u.completedOrders = append(u.completedOrders, *order)
+							u.completedOrders = append(u.completedOrders, sellOrd)
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if sellOrd.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
 						}
 					}
-					fmt.Printf("Buy order complete: bought %v UAH for %v USD each.\n", order.amount, sellOrd.price)
-					fmt.Printf("Sell order complete: sold %v UAH for %v USD each.\n", order.amount, sellOrd.price)
+					fmt.Printf("\nBuy order complete: bought %v UAH for %v USD each.\n", order.amount, sellOrd.price)
+					fmt.Printf("\nSell order complete: sold %v UAH for %v USD each.\n", order.amount, sellOrd.price)
 					ob.DelOrder(*order)
 					ob.DelOrder(sellOrd)
 					ob.print()
@@ -115,20 +157,36 @@ func (order *order) MatchOrders(ob *orderBook, users *users) {
 						if u.user_id == order.user_id {
 							u.assets["UAH"] += sellOrd.amount
 							u.assets["USD"] -= total_price
-						}
-						if u.user_id == sellOrd.user_id {
+							order.amount = remaining_amount
+							for ind, o := range (*users)[i].activeOrders {
+								if o.order_id == order.order_id {
+									(*users)[i].activeOrders[ind] = *order
+								}
+							}
+						} else if u.user_id == sellOrd.user_id {
 							u.assets["USD"] += total_price
 							u.assets["UAH"] -= sellOrd.amount
 							u.completedOrders = append(u.completedOrders, sellOrd)
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if sellOrd.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
 						}
 					}
 					order.amount = remaining_amount
 					for i, ord := range ob.BuyOrders {
-						if ord.order_id == ord.order_id {
+						if ord.order_id == order.order_id {
 							ob.BuyOrders[i] = *order
 						}
 					}
-					fmt.Printf("Sell order complete: sold %v UAH for %v USD each.\n", sellOrd.amount, sellOrd.price)
+					fmt.Printf("\nBuy order complete: bought %v UAH for %v USD each.\n", sellOrd.amount, sellOrd.price)
 					ob.DelOrder(sellOrd)
 					completed_orders++
 					ob.print()
@@ -141,40 +199,81 @@ func (order *order) MatchOrders(ob *orderBook, users *users) {
 				if order.amount < buyOrd.amount {
 					remaining_amount := buyOrd.amount - order.amount
 					total_price := buyOrd.price * order.amount
-					for _, u := range *users { // changing assets
+					for k, u := range *users { // changing assets
 						if u.user_id == order.user_id {
 							u.assets["UAH"] -= order.amount
 							u.assets["USD"] += total_price
 							u.completedOrders = append(u.completedOrders, *order)
+							(*users)[k].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if order.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[k].activeOrders = u.activeOrders
 						}
 						if u.user_id == buyOrd.user_id {
 							u.assets["USD"] -= total_price
 							u.assets["UAH"] += order.amount
+							buyOrd.amount = remaining_amount
+							for ind, o := range (*users)[k].activeOrders {
+								if o.order_id == buyOrd.order_id {
+									(*users)[k].activeOrders[ind] = buyOrd
+								}
+							}
 						}
 					}
-					buyOrd.amount = remaining_amount
-					ob.BuyOrders[i-completed_orders] = buyOrd
-					fmt.Printf("Sell order complete: sold %v UAH for %v USD each.\n", order.amount, buyOrd.price)
+					if len(ob.BuyOrders) > 1 {
+						ob.BuyOrders[i-completed_orders] = buyOrd
+					} else {
+						ob.BuyOrders[0] = buyOrd
+					}
+					fmt.Printf("\nSell order complete: sold %v UAH for %v USD each.\n", order.amount, buyOrd.price)
 					ob.DelOrder(*order)
 					completed_orders++
 					ob.print()
 					break
 				} else if order.amount == buyOrd.amount {
 					total_price := buyOrd.price * order.amount
-					for _, u := range *users { // changing assets
+					for i, u := range *users { // changing assets
 						if u.user_id == order.user_id {
 							u.assets["UAH"] -= order.amount
 							u.assets["USD"] += total_price
 							u.completedOrders = append(u.completedOrders, *order)
-						}
-						if u.user_id == buyOrd.user_id {
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if order.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
+						} else if u.user_id == buyOrd.user_id {
 							u.assets["USD"] -= total_price
 							u.assets["UAH"] += order.amount
-							u.completedOrders = append(u.completedOrders, *order)
+							u.completedOrders = append(u.completedOrders, buyOrd)
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if buyOrd.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
 						}
 					}
-					fmt.Printf("Sell order complete: sold %v UAH for %v USD each.\n", order.amount, buyOrd.price)
-					fmt.Printf("Buy order complete: bought %v UAH for %v USD each.\n", order.amount, buyOrd.price)
+					fmt.Printf("\nSell order complete: sold %v UAH for %v USD each.\n", order.amount, buyOrd.price)
+					fmt.Printf("\nBuy order complete: bought %v UAH for %v USD each.\n", order.amount, buyOrd.price)
 					ob.DelOrder(*order)
 					ob.DelOrder(buyOrd)
 					ob.print()
@@ -182,24 +281,39 @@ func (order *order) MatchOrders(ob *orderBook, users *users) {
 				} else {
 					remaining_amount := order.amount - buyOrd.amount
 					total_price := buyOrd.price * buyOrd.amount
-					for _, u := range *users { // changing assets
+					for i, u := range *users { // changing assets
 						if u.user_id == order.user_id {
 							u.assets["UAH"] -= buyOrd.amount
 							u.assets["USD"] += total_price
-						}
-						if u.user_id == buyOrd.user_id {
+							order.amount = remaining_amount
+							for ind, o := range (*users)[i].activeOrders {
+								if o.order_id == order.order_id {
+									(*users)[i].activeOrders[ind] = *order
+								}
+							}
+						} else if u.user_id == buyOrd.user_id {
 							u.assets["USD"] -= total_price
 							u.assets["UAH"] += buyOrd.amount
-							u.completedOrders = append(u.completedOrders, *order)
+							u.completedOrders = append(u.completedOrders, buyOrd)
+							(*users)[i].completedOrders = u.completedOrders
+							if len(u.activeOrders) == 1 {
+								u.activeOrders = u.activeOrders[0:0]
+							} else {
+								for ind, o := range u.activeOrders {
+									if buyOrd.order_id == o.order_id {
+										u.activeOrders = append(u.activeOrders[:ind], u.activeOrders[ind+1:]...)
+									}
+								}
+							}
+							(*users)[i].activeOrders = u.activeOrders
 						}
 					}
-					order.amount = remaining_amount
 					for i, ord := range ob.SellOrders {
-						if ord.order_id == ord.order_id {
+						if ord.order_id == order.order_id {
 							ob.SellOrders[i] = *order
 						}
 					}
-					fmt.Printf("Buy order complete: bought %v UAH for %v USD each.\n", buyOrd.amount, buyOrd.price)
+					fmt.Printf("\nBuy order complete: bought %v UAH for %v USD each.\n", buyOrd.amount, buyOrd.price)
 					ob.DelOrder(buyOrd)
 					ob.print()
 				}
@@ -210,10 +324,11 @@ func (order *order) MatchOrders(ob *orderBook, users *users) {
 
 func (ob *orderBook) AddOrder(order order, users *users) {
 	order.order_id = *incrPtr
+	order.init_amount = order.amount
 	enough_assets := false
 	for _, v := range *users {
 		if v.user_id == order.user_id {
-			if order.side == true {
+			if order.side {
 				if order.amount*order.price <= v.assets["USD"] {
 					enough_assets = true
 					break
@@ -243,13 +358,13 @@ func (ob *orderBook) AddOrder(order order, users *users) {
 					})
 					(*users)[i].activeOrders = append((*users)[i].activeOrders, order)
 					*incrPtr++
-					fmt.Printf("Added buy order: order_id - %v, user_id - %v, amount - %v, price - %v.\n", order.order_id, order.user_id, order.amount, order.price)
+					fmt.Printf("\nAdded buy order: order_id - %v, user_id - %v, amount - %v, price - %v.\n", order.order_id, order.user_id, order.amount, order.price)
 					flag = true
 					break
 				}
 			}
 			if !flag {
-				fmt.Println("Error, there is no user with such user_id. Can't add order to order book.")
+				fmt.Println("\nError, there is no user with such user_id. Can't add order to order book.")
 			}
 		} else {
 			flag := false
@@ -261,13 +376,13 @@ func (ob *orderBook) AddOrder(order order, users *users) {
 					})
 					(*users)[i].activeOrders = append((*users)[i].activeOrders, order)
 					*incrPtr++
-					fmt.Printf("Added sell order: order_id - %v, user_id - %v, amount - %v, price - %v.\n", order.order_id, order.user_id, order.amount, order.price)
+					fmt.Printf("\nAdded sell order: order_id - %v, user_id - %v, amount - %v, price - %v.\n", order.order_id, order.user_id, order.amount, order.price)
 					flag = true
 					break
 				}
 			}
 			if !flag {
-				fmt.Println("Error, there is no user with such user_id. Can't add order to order book.")
+				fmt.Println("\nError, there is no user with such user_id. Can't add order to order book.")
 			}
 		}
 		ob.print()
@@ -277,7 +392,8 @@ func (ob *orderBook) AddOrder(order order, users *users) {
 
 func (ob *orderBook) print() {
 	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
-	fmt.Fprintln(writer, "Size\t\tBid\t\tAsk\t\tSize")
+	fmt.Fprintln(writer, "\nSize\t\tBid\t\tAsk\t\tSize")
+	fmt.Fprintf(writer, "---------------------------------------------------------\n")
 	if diff := len(ob.BuyOrders) - len(ob.SellOrders); diff <= 0 {
 		ind := 0
 		if len(ob.BuyOrders) != 0 {
@@ -314,22 +430,41 @@ func (ob *orderBook) print() {
 
 func (b *balance) print() {
 	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 1, '\t', tabwriter.AlignRight)
-	fmt.Fprintf(writer, "User_id (%v) assets:\t|USD - %v|\t|UAH - %v|\n", b.user_id, b.assets["USD"], b.assets["UAH"])
+	fmt.Fprintf(writer, "---------------------------------------------------------\n")
+	fmt.Fprintf(writer, "User_id (%v) assets:\n|USD - %v|\t|UAH - %v|\n\n", b.user_id, b.assets["USD"], b.assets["UAH"])
+	fmt.Fprintf(writer, "User_id (%v) active orders:\n", b.user_id)
+	for _, o := range b.activeOrders {
+		if o.side {
+			fmt.Fprintf(writer, "Buy order:\t|order_id: %v|\t|size: %v|\t|bid: %v|\n", o.order_id, o.amount, o.price)
+		} else {
+			fmt.Fprintf(writer, "Sell order:\t|order_id: %v|\t|size: %v|\t|ask: %v|\n", o.order_id, o.amount, o.price)
+		}
+	}
+	fmt.Fprintf(writer, "\nUser_id (%v) completed orders:\n", b.user_id)
+	for _, o := range b.completedOrders {
+		if o.side {
+			fmt.Fprintf(writer, "Buy order:\t|order_id: %v|\t|size: %v|\t|bid: %v|\n", o.order_id, o.init_amount, o.price)
+		} else {
+			fmt.Fprintf(writer, "Sell order:\t|order_id: %v|\t|size: %v|\t|ask: %v|\n", o.order_id, o.init_amount, o.price)
+		}
+	}
+	//fmt.Fprintf(writer, "---------------------------------------------------------\n")
 	writer.Flush()
 }
 
 func main() {
 	orderB := orderBook{}
 	users := users{}
-	users.ModifyBalance(1, "USD", 100)
-	users.ModifyBalance(2, "USD", 100)
-	users.ModifyBalance(3, "UAH", 100)
+	users.ModifyBalance(1, "UAH", 100)
+	users.ModifyBalance(2, "UAH", 100)
+	users.ModifyBalance(3, "USD", 100)
+	users[0].print()
 	users[1].print()
 	users[2].print()
-	orderB.AddOrder(order{user_id: 1, amount: 9, price: 18, side: true}, &users)
-	orderB.AddOrder(order{user_id: 2, amount: 10, price: 6, side: true}, &users)
-	orderB.AddOrder(order{user_id: 3, amount: 15, price: 5, side: false}, &users)
-	orderB.AddOrder(order{user_id: 3, amount: 7, price: 6, side: true}, &users)
+	orderB.AddOrder(order{user_id: 1, amount: 10, price: 5, side: false}, &users)
+	orderB.AddOrder(order{user_id: 2, amount: 5, price: 6, side: false}, &users)
+	orderB.AddOrder(order{user_id: 3, amount: 15, price: 6, side: true}, &users)
+	users[0].print()
 	users[1].print()
 	users[2].print()
 	//orderB.DelOrder(orderB.BuyOrders[0])
